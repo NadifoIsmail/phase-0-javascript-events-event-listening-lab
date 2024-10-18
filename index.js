58e7ea729a33fe6d005c37fba977bceb16a5bd89
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-18 23:30:29 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    input.addEventListener("click", ()=> {
+        alert("Button clicked!");
+    });
 }
+
+addingEventListener();
+
+
